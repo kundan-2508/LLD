7c6178289c4af0329085d0ba2064bb5b6c5bbb2b
kundan-2508 <kun123658@gmail.com>
2024-05-06 07:20:45 +0530

    Completed Factory Pattern and Abstract Factory Pattern

---
+++ FactoryVsAbstract/AbstractFactoryDesignPattern/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace AbstractFactoryDesignPattern
+{
+    public interface ICar
+    {
+        void Manufacture();
+    }
+
+    public interface IBike
+    {
+        void Manufacture();
+    }
+
+    public class TataCar : ICar
+    {
+        public void Manufacture()
+        {
+            Console.WriteLine("Tata car manufacture");
+        }
+    }
+
+    public class TataBike : IBike
+    {
+        public void Manufacture()
+        {
+            Console.WriteLine("Tata bike manufacture");
+        }
+    }
+
+    public class TeslaCar : ICar
+    {
+        public void Manufacture()
+        {
+            Console.WriteLine("Tesla car manufacture");
+        }
+    }
+
+    public class TeslaBike : IBike
+    {
+        public void Manufacture()
+        {
+            Console.WriteLine("Tesla bike manufacture");
+        }
+    }
+
+    //Abstract factory class
+    public abstract class VechileCompany
+    {
+        public abstract ICar GetCar();
+        public abstract IBike GetBike();
+    }
+
+    public class TeslaCompany : VechileCompany
+    {
+        public override IBike GetBike()
+        {
+            return new TeslaBike();
+        }
+
+        public override ICar GetCar()
+        {
+            return new TeslaCar();
+        }
+    }
+
+    public class TataCompany : VechileCompany
+    {
+        public override IBike GetBike()
+        {
+            return new TataBike();
+        }
+
+        public override ICar GetCar()
+        {
+            return new TataCar();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            VechileCompany tata = new TataCompany();
+            VechileCompany tesla = new TeslaCompany();
+            tata.GetBike().Manufacture();
+            tesla.GetBike().Manufacture();
+            tata.GetCar().Manufacture();
+            tesla.GetCar().Manufacture();
+        }
+    }
+}
---
+++ FactoryVsAbstract/FactoryDesignPattern/Program.cs
@@ -0,0 +1,83 @@
+﻿//namespace FactoryDesignPattern
+//{
+//    public interface Icar
+//    {
+//        void start();
+//    }
+//    public class SixSeaterCar : Icar
+//    {
+//        public void start()
+//        {
+//            Console.WriteLine("Six Seater car sstarted");
+//        }
+//    }
+//    public class FourSeaterCar : Icar
+//    {
+//        public void start()
+//        {
+//            Console.WriteLine("Four Seater car sstarted");
+//        }
+//    }
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Icar SixSeater = new SixSeaterCar();
+//            Icar FourSeater = new FourSeaterCar();
+//            SixSeater.start();
+//            FourSeater.start();
+//        }
+//    }
+//}
+
+//above code is not good, we can implement the same thing using factory design pattern
+
+namespace FactoryDesignPattern
+{
+    public interface ICar
+    {
+        void Start();
+    }
+
+    public class SixSeater : ICar
+    {
+        public void Start()
+        {
+            Console.WriteLine("Six Seater car started");
+        }
+    }
+
+    public class FourSeater : ICar
+    {
+        public void Start()
+        {
+            Console.WriteLine("Four Seater car started");
+        }
+    }
+
+    public class CarFactory
+    {
+        public ICar getCar(string type)
+        {
+            switch (type)
+            {
+                case "four":
+                    return new FourSeater();
+                case "six":
+                    return new SixSeater();
+            }
+            return null;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarFactory carFactory = new CarFactory();
+            ICar fourSeater = carFactory.getCar("four");
+            ICar sixSeater = carFactory.getCar("six");
+            sixSeater.Start();
+            fourSeater.Start();
+        }
+    }
+}
