6ebca9f96443b4a9ebe16d62875025e9f7fe9b0e
kundan-2508 <kun123658@gmail.com>
2024-05-06 08:20:30 +0530

    Completed Iterator Design Pattern

---
+++ IteratorDesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace IteratorDesignPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+//It is a behavioural design pattern, that provides a way to access element of a collection sequentially without exposing
+//the underlying representation of the collection
